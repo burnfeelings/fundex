1f197bb9ef0d86ffe7de8888de0228c21c3a61ed
burnfeelings <44791313+burnfeelings@users.noreply.github.com>
2024-12-12 14:11:53 -0400

    Update src/App.tsx [BoltSync]

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Hero } from './components/Hero';
 import { Features } from './components/Features';
 import { Pricing } from './components/Pricing';
 import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Features />
       <Pricing />
       <Contact />
+      <Footer />
     </div>
   );
 }
